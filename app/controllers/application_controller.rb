1a499bab831584aa712290afcb2649796f292efa
arnold-domaya <domaya_arnold@yahoo.fr>
2018-02-13 12:37:57 +0100

    heroku comp

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  render html: "hello word Devise!"
+  def home
+    render html: "hello word Devise!"
+  end 
 end
